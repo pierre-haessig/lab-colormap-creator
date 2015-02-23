7c021813c94625cfc199afd460b9dbaf003a8892
Pierre Haessig <pierre.haessig@crans.org>
2015-02-23 13:00:23 +0100

    first working version of plotting Lab space cuts

--- color_creator.py
+++ color_creator.py
@@ -32,18 +32,60 @@ for i in range(len(x0)):
     )
 
 
-na, nb =(20, 21)
-L = 55.
-a_min,a_max = (-0.7,1.)
-b_min, b_max = (-0.9,0.8)
+# Global extent of the ab plane
+a_min,a_max = (-100.,100.)
+b_min, b_max = (-110.,100.)
 
-lab_grid = np.zeros((nb,na,3))
-lab_grid[:,:,0] = L
-b_grid, a_grid = np.ogrid[b_min:b_max:1j*nb,
-                          a_min:a_max:1j*na]
-lab_grid[:,:,1] = a_grid
-lab_grid[:,:,2] = b_grid
+ab_imparams = dict(origin='lower', extent=[a_min, a_max, b_min, b_max],
+                   interpolation='nearest', cmap='gray')
 
-rgb_grid = color.lab2rgb(lab_grid)
+
+def ab_plane(L, na, nb):
+    '''Generate a sRBG image of the ab plane at luminance `L`
+    
+    a increases along the axis 1, while
+    b increases along axis 0.
+    '''
+    # Generate the ab grid
+    ab_grid_lab = np.zeros((nb,na,3))
+    ab_grid_lab[:,:,0] = L
+    b_grid, a_grid = np.ogrid[b_min:b_max:1j*nb,
+                              a_min:a_max:1j*na]
+    ab_grid_lab[:,:,1] = a_grid
+    ab_grid_lab[:,:,2] = b_grid
+
+    # Convert Lab -> sRBG
+    ab_grid_rgb = color.lab2rgb(ab_grid_lab)
+
+    # Detect saturation in the RGB space:
+    sat = ((ab_grid_rgb<0) | (ab_grid_rgb>1)).sum(axis=-1).astype(bool)
+    #plt.imshow(sat, **ab_imparams)
+
+    # Apply channel saturation
+    ab_grid_rgb[ab_grid_rgb>1] = 1
+    ab_grid_rgb[ab_grid_rgb<0] = 0
+
+    # Gray out the saturated zones
+    ab_grid_rgb[sat] = 0.5 + (ab_grid_rgb[sat]-0.5)*0.2
+    
+    return ab_grid_rgb
+
+
+def plot_ab_plane(L):
+    '''plot the output of `ab_plane` generation function'''
+    ab_grid_rgb = ab_plane(L, 300, 300)
+    plt.imshow(ab_grid_rgb, **ab_imparams)
+    # add a small cross at the center (neutral gray)
+    plt.plot(0,0,'+', color='w' if L<50 else 'k')
+    # annotations:
+    ax = plt.gca()
+    ax.set(
+        xlabel = 'a', ylabel = 'b',
+        xlim = (a_min,a_max),
+        ylim = (b_min,b_max),
+        title = 'Lab space cut at L={:.0f}'.format(L)
+        )
+    plt.tight_layout()
+    
 
 plt.show()
